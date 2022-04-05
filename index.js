84fe2a53fba04c29bf8c050d891351a91c19c247
William Luck <luck.william.h@gmail.com>
2022-04-05 11:11:07 -0500

    commit tree ac35324505f968bc598ca9d1a510a4f3280dbce8

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = word.split('').reverse().join('');
+  if (reversedWord === word) {
+    return true
+  } else {
+    return false
+  }
 }
 
+// let reversedWord = isPalindrome('racecar')
+// console.log('Expecting: racecar')
+// console.log(reversedWord)
+
+// reversedWord = isPalindrome('coffee')
+// console.log('Expecting: eeffoc')
+// console.log(reversedWord)
+
 /* 
-  Add your pseudocode here
+
+Pseudocode goes here: 
+
+ Takes in a string, then checks the length.
+  Index 0 to index string.length
+  Index 1 to index string.length-1
+  Dont' really know how to do this 
+
+Iterate though array backwards?
+  Populate new array with each letter, then convert that array to string. 
+  Check if new string and original string match.
+
+  There are some built-in methods to use for this, but have to use all three
+    split(): splits a string into an array of substrings using a seperator
+    reverse(): Reverses the order the elemnets in an array. 
+    join(): Jions all elemnts of an array inot a string, returns the new string. 
+
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here: 
+  
+  Functions take in a string. 
+  The funciton first splits the string into an array
+  Then reverses that array
+  Then joins that array back into a string. 
+
+  The function then tests if the new string is equal to the original string 
+  Will return true if it does equal, and will return false otherwise. 
+
 */
 
 // You can run `node index.js` to view these console logs
